041f95cabc7fadf85781c7dc95a38a6b8089bff9
Heloise Dafforne <17DaffoH@fulford.york.sch.uk>
2023-02-01 12:38:27 +0000

    added somhting

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,45 @@ namespace Server
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Log In server");
+
+            const int port = 80;
+            string prefix = $"http://localhost:{port}/";
+
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add(prefix);
+            server.Start();
+            HttpListenerContext context = server.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            string html = "Hello World";
+
+            switch (request.RawUrl)
+            {
+                case "/":
+                    html = File.ReadAllText("../../static/index.html");
+                    break;
+                default:
+                    string path = "../../static" + request.RawUrl;
+                    if (File.Exists(path))
+                    {
+                        html = File.ReadAllText(path);
+                    }
+                    else
+                    {
+                        response.StatusCode = 404;
+                        html = "sorry - file not found :(";
+                        Console.WriteLine($"Unknown URL: {request.RawUrl}");
+                    }
+                    break;
+            }
+
+            byte[] buffer = Encoding.UTF8.GetBytes(html);
+            Console.WriteLine($"Sending {buffer.Length} bytes. . .");
+
+            response.ContentLength64 = buffer.Length;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            server.Stop();
+
             Console.ReadLine();
         }
     }
---
+++ Server/lib.js
@@ -0,0 +1,33 @@
+﻿const eHints = document.getElementById("hints");
+
+function reset() {
+
+}
+
+function createUser() {
+
+}
+
+function login() {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    presenceCheck(username, "Enter a valid username");
+    presenceCheck(password, "Enter a valid password");
+    eHints.innerText = "Attempting login..."
+    lengthCheck(password, 8, 12, "Please enter a passowrd between 8 and 12 characters")
+}
+
+function presenceCheck(input, message) {
+
+    if (input == "") {
+        eHints.innerText = message;
+        throw message;
+    }
+}
+
+function lengthCheck(input, minLength, maxLength, message) {
+    if (input.length > maxLength || input.length < minLength) {
+        eHints.innerText = message;
+        throw message;
+    }
+}
